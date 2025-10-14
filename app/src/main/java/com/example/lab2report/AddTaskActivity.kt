4dc6295e5f88ce710fc477368b59e11e1ab852d1
jonathanhua <jonathanhua@hotmail.com>
2025-10-14 10:58:29 -0400

    Added Colors and error check for empty Title

--- app/src/main/java/com/example/lab2report/AddTaskActivity.kt
+++ app/src/main/java/com/example/lab2report/AddTaskActivity.kt
@@ -8,6 +8,10 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Calendar
+import android.widget.Toast
+import android.view.View
+
+
 
 class AddTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,36 @@ class AddTaskActivity : AppCompatActivity() {
         val etDeadline = findViewById<EditText>(R.id.etDeadline)
         val btnBack = findViewById<Button>(R.id.btnBack)
         val btnDone = findViewById<Button>(R.id.btnDone)
+        val etTaskTitle = findViewById<EditText>(R.id.etTaskTitle)
+
+        //  colors
+        var selectedColor: Int = Color.WHITE // default color
+        val Red = findViewById<View>(R.id.colorRed)
+        val Green = findViewById<View>(R.id.colorGreen)
+        val Blue = findViewById<View>(R.id.colorBlue)
+        val Yellow = findViewById<View>(R.id.colorYellow)
+
+        Red.setOnClickListener {
+            selectedColor = Color.RED
+            Toast.makeText(this,"You have selected Red",Toast.LENGTH_SHORT).show()
+        }
+
+        Green.setOnClickListener {
+            selectedColor = Color.GREEN
+            Toast.makeText(this,"You have selected Green",Toast.LENGTH_SHORT).show()
+        }
+
+        Blue.setOnClickListener {
+            selectedColor = Color.BLUE
+            Toast.makeText(this,"You have selected Blue",Toast.LENGTH_SHORT).show()
+        }
+
+        Yellow.setOnClickListener {
+            selectedColor = Color.YELLOW
+            Toast.makeText(this,"You have selected Yellow",Toast.LENGTH_SHORT).show()
+        }
+
+
 
         // Style circular buttons
         styleCircularButton(btnBack, Color.parseColor("#2196F3"), Color.BLACK)
@@ -35,6 +69,7 @@ class AddTaskActivity : AppCompatActivity() {
             dpd.show()
         }
 
+
         // Back button
         btnBack.setOnClickListener {
             finish()
@@ -42,8 +77,17 @@ class AddTaskActivity : AppCompatActivity() {
 
         // Done button
         btnDone.setOnClickListener {
-            finish()
+            val etTaskTitleStr = etTaskTitle.text.toString()
+            if(etTaskTitleStr.isEmpty()){
+                Toast.makeText(this,"Please add a title for task",Toast.LENGTH_SHORT).show()
+                etTaskTitle.error = "Title is required"
+            }
+            else{
+                finish()
+            }
         }
+
+
     }
 
     private fun styleCircularButton(button: Button, fillColor: Int, strokeColor: Int) {
@@ -55,4 +99,5 @@ class AddTaskActivity : AppCompatActivity() {
         button.background = circle
         button.setTextColor(Color.WHITE)
     }
+
 }
